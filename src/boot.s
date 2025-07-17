fb5a5c536d2a2d6ed1a8d35d83f997ce3b9b87f1
Brick Sigma <bricksigma176@gmail.com>
2025-07-17 23:26:58 +0300

    Added register printing and text cursor wrapping for VGA buffer

--- src/boot.s
+++ src/boot.s
@@ -6,21 +6,24 @@
 _start:
     # Initialize the DS register and stack
     xor %ax, %ax
-    mov %ax, %ds  # DS = 0
-    mov %ax, %ss  # Stack starts at 0
-    mov $0x9c00, %sp  # Stack pointer starts 2000h 
+    movw %ax, %ds  # DS = 0
+    movw %ax, %ss  # Stack starts at 0
+    movw $0x9c00, %sp  # Stack pointer starts 2000h 
 
     cld
 
     # Setup the video memory pointer in ES
-    mov $0xb800, %ax
-    mov %ax, %es
+    movw $0xb800, %ax
+    movw %ax, %es
 
-    mov $msg, %si  # Move the address of the message into the src index register
+    movw $msg, %si  # Move the address of the message into the src index register
     call sprint
 
-    mov $msg2, %si
+    movw $msg2, %si
     call sprint
+
+    movw $0x5e2f, reg16
+    call printreg16
 
 hang:
     jmp hang
@@ -29,7 +32,7 @@ hang:
 
 # Data section
 msg: .ascii "Hello World!\x0"
-msg2: .ascii "This is being printed without BIOS interrupts!\x0"
+msg2: .ascii "This is being printed without BIOS interrupts! I also have a lot more text added to see if it'll wrap around the screen, let's see if it does it!!!\x0"
 
     # Padding the end of the bootloader
     .fill 510 - (. - _start)
--- src/print.s
+++ src/print.s
@@ -3,14 +3,23 @@
 
 .section .text
 .global sprint
+sprint:
+    push %cx  # save CX
+    jmp sprint_loop
 dochar:
     call cprint  # Print one character
-sprint:
+sprint_loop:
     lodsb  # String into AL
     cmp $0, %al  # Check if at end of string
     jne dochar
     addb $1, ypos  # Move to the next line in video memory
+    cmpb $25, ypos
+    jb sprint_end  # Check if ypos >= 25
+    movb $0, ypos  # Set ypos to 0 if it is >= 25
+sprint_end:
     movb $0, xpos  # Move left
+
+    pop %cx  # Restore CX
     ret
 
 cprint:
@@ -29,9 +38,49 @@ cprint:
     movw %cx, %ax
     stosw
     addb $1, xpos
+    cmpb $80, xpos
+    jb cprint_end
+    # If the xpos goes beyond the screen, go to the next line.
+    movb $0, xpos
+    addb $1, ypos  # Move to the next line in video memory
+    cmpb $25, ypos
+    jb cprint_end  # Check if ypos >= 25
+    movb $0, ypos  # Set ypos to 0 if it is >= 25
+cprint_end:
+    ret
+
+.global printreg16
+printreg16:
+    push %es # Save the old value of ES onto the stack
+    movw %ds, %ax
+    movw %ax, %es  # Copy DS into ES
+
+    movw $outstr16, %di  # Load the pointer for the output string
+    movw reg16, %ax  # Copy the value of reg16 into AX
+    movw $hexstr, %si  # Load the pointer of hexstr
+    movw $4, %cx  # Used to loop over every hex character
+hexloop:
+    rol $4, %ax  # Get the leftmost bits (5E2F --> E2F5)
+    movw %ax, %bx
+    andw $0x0f, %bx  # Get only the last 4 bits/the last hex value (E2F5 --> 0005)
+    movb (%bx,%si), %al  # Index into hexstr and store the character in AL
+    stosb  # Write the string value into outreg16
+    decw %cx
+    jnz hexloop
+
+    pop %es  # Restore the old ES value
+
+    movw $outstr16, %si
+    call sprint
 
     ret
 
 # Internal data variables
 xpos: .byte 0
 ypos: .byte 0
+
+hexstr: .ascii "0123456789ABCDEF"
+outstr16: .asciz "0000"  # Hold the string value of the register value
+
+.global reg16
+reg16: .word 0  # Used for passing values to printreg16
